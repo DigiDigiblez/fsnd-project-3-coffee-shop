e46586160d3da44f3e006d66f67644f300da03c6
Carl Bowen <carl.bowen@vibrantmedia.com>
2020-04-28 04:32:02 +0100

    Adding the authentication. Housekeeping, removing task comments/todos and replacing single with double quotes.

--- backend/src/api.py
+++ backend/src/api.py
@@ -4,7 +4,7 @@ import sys
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 
-from .auth.auth import AuthError, CODE, abort_with_error
+from .auth.auth import AuthError, RESPONSE_CODE
 from .database.models import db_drop_and_create_all, setup_db, Drink
 
 app = Flask(__name__)
@@ -20,13 +20,13 @@ def abort_with_error(err_code):
 
 
 # Get all drinks
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
     return "Open for business!"
 
 
 # Get basic drink details
-@app.route('/drinks', methods=['GET'])
+@app.route("/drinks", methods=["GET"])
 def get_drinks():
     try:
         drinks = Drink.query.all()
@@ -37,16 +37,16 @@ def get_drinks():
             "drinks": short_drinks
         }
 
-        return jsonify(response), CODE["200_OK"]
+        return jsonify(response), RESPONSE_CODE["200_OK"]
 
     except:
         print(sys.exc_info())
-        abort_with_error(CODE["500_INTERNAL_SERVER_ERROR"])
+        abort_with_error(RESPONSE_CODE["500_INTERNAL_SERVER_ERROR"])
 
 
 # Get full drink details
-@app.route('/drinks-detail', methods=["GET"])
-# @requires_auth('get:drinks-detail')
+@app.route("/drinks-detail", methods=["GET"])
+# @requires_auth("get:drinks-detail")
 def get_drinks_detail():
     try:
         drinks = Drink.query.all()
@@ -57,21 +57,21 @@ def get_drinks_detail():
             "drinks": long_drinks
         }
 
-        return jsonify(response), CODE["200_OK"]
+        return jsonify(response), RESPONSE_CODE["200_OK"]
 
     except:
         print(sys.exc_info())
-        abort_with_error(CODE["500_INTERNAL_SERVER_ERROR"])
+        abort_with_error(RESPONSE_CODE["500_INTERNAL_SERVER_ERROR"])
 
 
 # POST a new drink
 @app.route("/drinks", methods=["POST"])
-# TODO - should require the 'post:drinks' permission
+# TODO - should require the "post:drinks" permission
 def post_drink():
     body = request.get_json()
 
     if "title" not in body or "recipe" not in body:
-        abort_with_error(CODE["422_UNPROCESSABLE_ENTITY"])
+        abort_with_error(RESPONSE_CODE["422_UNPROCESSABLE_ENTITY"])
 
     drink_title = body.get("title", None)
     drink_recipe = body.get("recipe", None)
@@ -90,33 +90,23 @@ def post_drink():
         "drinks": [new_drink.short()],
     }
 
-    return jsonify(response), CODE["200_OK"]
-
-
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        it should update the corresponding row for <id>
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+    return jsonify(response), RESPONSE_CODE["200_OK"]
 
 
 # Update a drink
-# TODO - should require the 'patch:drinks' permission
+# TODO - should require the "patch:drinks" permission
 @app.route("/drinks/<int:drink_id>", methods=["PATCH"])
 def patch_drink(drink_id):
     try:
         # Error handling for id
         drink_count = len(Drink.query.all())
         if drink_id is None or drink_id <= 0 or drink_id > drink_count:
-            abort_with_error(CODE["404_RESOURCE_NOT_FOUND"])
+            abort_with_error(RESPONSE_CODE["404_RESOURCE_NOT_FOUND"])
 
         # Error handling for existing drink
         existing_drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
         if existing_drink is None:
-            abort_with_error(CODE["404_RESOURCE_NOT_FOUND"])
+            abort_with_error(RESPONSE_CODE["404_RESOURCE_NOT_FOUND"])
 
         # Retrieving values for updating
         body = request.get_json()
@@ -144,26 +134,26 @@ def patch_drink(drink_id):
             "drinks": [existing_drink.long()],
         }
 
-        return jsonify(response), CODE["200_OK"]
+        return jsonify(response), RESPONSE_CODE["200_OK"]
 
     except:
         print(sys.exc_info())
-        abort_with_error(CODE["500_INTERNAL_SERVER_ERROR"])
+        abort_with_error(RESPONSE_CODE["500_INTERNAL_SERVER_ERROR"])
 
 
 # Delete a drink
-# TODO - should require the 'delete:drinks' permission
+# TODO - should require the "delete:drinks" permission
 @app.route("/drinks/<int:drink_id>", methods=["DELETE"])
 def delete_drink(drink_id):
     drink_count = len(Drink.query.all())
 
     if drink_id is None or drink_id <= 0 or drink_id > drink_count:
-        abort_with_error(CODE["404_RESOURCE_NOT_FOUND"])
+        abort_with_error(RESPONSE_CODE["404_RESOURCE_NOT_FOUND"])
 
     target_drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
 
     if target_drink is None:
-        abort_with_error(CODE["404_RESOURCE_NOT_FOUND"])
+        abort_with_error(RESPONSE_CODE["404_RESOURCE_NOT_FOUND"])
 
     target_drink.delete()
 
@@ -172,73 +162,63 @@ def delete_drink(drink_id):
         "delete": drink_id,
     }
 
-    return jsonify(response), CODE["200_OK"]
-
-
-'''
-@TODO implement endpoint
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it 
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
+    return jsonify(response), RESPONSE_CODE["200_OK"]
 
 
 # Error Handling
-@app.errorhandler(CODE["400_BAD_REQUEST"])
+@app.errorhandler(RESPONSE_CODE["400_BAD_REQUEST"])
 def bad_request(error):
     print(error)
 
     return jsonify({
         "success": False,
-        "error": CODE["400_BAD_REQUEST"],
+        "error": RESPONSE_CODE["400_BAD_REQUEST"],
         "message": "Bad request",
-    }), CODE["400_BAD_REQUEST"]
+    }), RESPONSE_CODE["400_BAD_REQUEST"]
 
 
-@app.errorhandler(CODE["404_RESOURCE_NOT_FOUND"])
+@app.errorhandler(RESPONSE_CODE["404_RESOURCE_NOT_FOUND"])
 def resource_not_found(error):
     print(error)
 
     return jsonify({
         "success": False,
-        "error": CODE["404_RESOURCE_NOT_FOUND"],
+        "error": RESPONSE_CODE["404_RESOURCE_NOT_FOUND"],
         "message": "Resource not found",
-    }), CODE["404_RESOURCE_NOT_FOUND"]
+    }), RESPONSE_CODE["404_RESOURCE_NOT_FOUND"]
 
 
-@app.errorhandler(CODE["405_METHOD_NOT_ALLOWED"])
+@app.errorhandler(RESPONSE_CODE["405_METHOD_NOT_ALLOWED"])
 def resource_not_found(error):
     print(error)
 
     return jsonify({
         "success": False,
-        "error": CODE["405_METHOD_NOT_ALLOWED"],
+        "error": RESPONSE_CODE["405_METHOD_NOT_ALLOWED"],
         "message": "Method not allowed",
-    }), CODE["405_METHOD_NOT_ALLOWED"]
+    }), RESPONSE_CODE["405_METHOD_NOT_ALLOWED"]
 
 
-@app.errorhandler(CODE["422_UNPROCESSABLE_ENTITY"])
+@app.errorhandler(RESPONSE_CODE["422_UNPROCESSABLE_ENTITY"])
 def unprocessable_entity(error):
     print(error)
 
     return jsonify({
         "success": False,
-        "error": CODE["422_UNPROCESSABLE_ENTITY"],
+        "error": RESPONSE_CODE["422_UNPROCESSABLE_ENTITY"],
         "message": "Unprocessable entity",
-    }), CODE["422_UNPROCESSABLE_ENTITY"]
+    }), RESPONSE_CODE["422_UNPROCESSABLE_ENTITY"]
 
 
-@app.errorhandler(CODE["500_INTERNAL_SERVER_ERROR"])
+@app.errorhandler(RESPONSE_CODE["500_INTERNAL_SERVER_ERROR"])
 def internal_server_error(error):
     print(error)
 
     return jsonify({
         "success": False,
-        "error": CODE["500_INTERNAL_SERVER_ERROR"],
+        "error": RESPONSE_CODE["500_INTERNAL_SERVER_ERROR"],
         "message": "Internal server error",
-    }), CODE["500_INTERNAL_SERVER_ERROR"]
+    }), RESPONSE_CODE["500_INTERNAL_SERVER_ERROR"]
 
 
 @app.errorhandler(AuthError)
--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -1,12 +1,15 @@
+import json
 from functools import wraps
+from urllib.request import urlopen
 
-from flask import request
+from flask import request, app
+from jose import jwt
 
-AUTH0_DOMAIN = 'fsnd2020.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'cafe'
+AUTH0_DOMAIN = "fsnd2020.auth0.com"
+ALGORITHMS = ["RS256"]
+API_AUDIENCE = "cafe"
 
-CODE = {
+RESPONSE_CODE = {
     # Success codes
     "200_OK": 200,
 
@@ -22,17 +25,11 @@ CODE = {
     "500_INTERNAL_SERVER_ERROR": 500
 }
 
-ERR = {
-    CODE["401_UNAUTHORIZED"]: {
-        "Type": "AuthError",
-        "Title": "invalid_header",
-        "Code": CODE["401_UNAUTHORIZED"],
-    },
-    CODE["403_FORBIDDEN"]: {
-        "Type": "AuthError",
-        "Title": "forbidden",
-        "Code": CODE["403_FORBIDDEN"],
-    }
+AUTH_HEADER_ERR = {
+    "INVALID_HEADER": "invalid_header",
+    "INVALID_CLAIMS": "invalid_claims",
+    "TOKEN_EXPIRED": "token_expired",
+    "UNAUTHORIZED": "unauthorized"
 }
 
 
@@ -43,98 +40,128 @@ class AuthError(Exception):
         self.status_code = status_code
 
 
-# Handles the raising of errors
-def raise_error(err, err_desc: str):
-    if err["Type"] == "AuthError":
-        raise AuthError({
-            "code": err["Title"],
-            "description": err_desc
-        }, err["Code"])
-
-
 # Retrieve token from auth header
 def get_token_auth_header():
     if "Authorization" not in request.headers:
-        # Raise a 401 error
-        raise_error(ERR[CODE["401_UNAUTHORIZED"]], "Authorization header is missing")
+        raise AuthError({
+            "code": AUTH_HEADER_ERR["INVALID_HEADER"],
+            "description": "Authorization header is missing"
+        }, RESPONSE_CODE["401_UNAUTHORIZED"])
 
     # Dissect header
-    header_parts = request.headers['Authorization'].split()
+    header_parts = request.headers["Authorization"].split()
     bearer_prefix = header_parts[0]
     auth_token = header_parts[1]
 
     # 401 err handling
     if not bearer_prefix:
-        raise_error(ERR[CODE["401_UNAUTHORIZED"]], "Authorization header is missing bearer prefix")
+        raise AuthError({
+            "code": AUTH_HEADER_ERR["INVALID_HEADER"],
+            "description": "Authorization header is missing bearer prefix"
+        }, RESPONSE_CODE["401_UNAUTHORIZED"])
 
     elif bearer_prefix and len(header_parts) == 1:
-        raise_error(ERR[CODE["401_UNAUTHORIZED"]], "Authorization header is missing auth token")
+        raise AuthError({
+            "code": AUTH_HEADER_ERR["INVALID_HEADER"],
+            "description": "Authorization header is missing auth token"
+        }, RESPONSE_CODE["401_UNAUTHORIZED"])
 
     elif len(header_parts) > 2:
-        raise_error(ERR[CODE["401_UNAUTHORIZED"]], "Authorization header is malformed")
+        raise AuthError({
+            "code": AUTH_HEADER_ERR["INVALID_HEADER"],
+            "description": "Authorization header is malformed"
+        }, RESPONSE_CODE["401_UNAUTHORIZED"])
 
     return auth_token
 
 
-'''
-@TODO implement check_permissions(permission, payload) method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
-        payload: decoded jwt payload
+def check_permissions(permission: str, payload):
+    # Check if user has any permissions
+    if "permissions" not in payload:
+        raise AuthError({
+            "code": AUTH_HEADER_ERR["INVALID_CLAIMS"],
+            "description": "User has no permissions"
+        }, RESPONSE_CODE["401_UNAUTHORIZED"])
 
-    it should raise an AuthError if permissions are not included in the payload
-        !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
-    return true otherwise
-'''
+    # Check if user has the requested permission
+    elif permission not in payload["permissions"]:
+        raise AuthError({
+            "code": AUTH_HEADER_ERR["UNAUTHORIZED"],
+            "description": "User doesn't have the requested permission"
+        }, RESPONSE_CODE["401_UNAUTHORIZED"])
 
-
-def check_permissions(permission, payload):
-    raise Exception('Not Implemented')
-
-
-'''
-@TODO implement verify_decode_jwt(token) method
-    @INPUTS
-        token: a json web token (string)
-
-    it should be an Auth0 token with key id (kid)
-    it should verify the token using Auth0 /.well-known/jwks.json
-    it should decode the payload from the token
-    it should validate the claims
-    return the decoded payload
-
-    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
-'''
+    return True
 
 
-def verify_decode_jwt(token):
-    raise Exception('Not Implemented')
+def verify_decode_jwt(token: str):
+    jsonurl = urlopen(f"https://{AUTH0_DOMAIN}/.well-known/jwks.json")
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+
+    if "kid" not in unverified_header:
+        raise AuthError({
+            "code": AUTH_HEADER_ERR["INVALID_HEADER"],
+            "description": "Authorization header is malformed"
+        }, RESPONSE_CODE["401_UNAUTHORIZED"])
+
+    for key in jwks["keys"]:
+        if key["kid"] == unverified_header["kid"]:
+            rsa_key = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"]
+            }
+
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer=f"https://{AUTH0_DOMAIN}/"
+            )
+
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                "code": AUTH_HEADER_ERR["TOKEN_EXPIRED"],
+                "description": "Token expired"
+            }, RESPONSE_CODE["401_UNAUTHORIZED"])
+
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                "code": AUTH_HEADER_ERR["INVALID_CLAIMS"],
+                "description": "Incorrect claims. Please, check the audience and issuer"
+            }, RESPONSE_CODE["401_UNAUTHORIZED"])
+
+        except Exception:
+            raise AuthError({
+                "code": AUTH_HEADER_ERR["INVALID_HEADER"],
+                "description": "Unable to parse authentication token"
+            }, RESPONSE_CODE["400_BAD_REQUEST"])
+
+    raise AuthError({
+        "code": AUTH_HEADER_ERR["INVALID_HEADER"],
+        "description": "Unable to find the appropriate key"
+    }, RESPONSE_CODE["400_BAD_REQUEST"])
 
 
-'''
-@TODO implement @requires_auth(permission) decorator method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
-
-    it should use the get_token_auth_header method to get the token
-    it should use the verify_decode_jwt method to decode the jwt
-    it should use the check_permissions method validate claims and check the requested permission
-    return the decorator which passes the decoded payload to the decorated method
-'''
-
-
-def requires_auth(permission=''):
+def requires_auth(permission=""):
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
-            # Retrieve jwt from header
+            # Retrieve jwt from auth header
             jwt = get_token_auth_header()
 
             # Grab the payload from decoded jwt
             payload = verify_decode_jwt(jwt)
 
-            # Check if user has permittance
+            # Check if user has any / requested permission(s)
             check_permissions(permission, payload)
 
             return f(payload, *args, **kwargs)
@@ -142,3 +169,10 @@ def requires_auth(permission=''):
         return wrapper
 
     return requires_auth_decorator
+
+
+@app.route("/headers")
+@requires_auth
+def headers(payload):
+    print(payload)
+    return "Access Granted!"
